039ac3f9118fde68c8177bde080bcd40b88f1eef
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2026-02-02 16:00:02 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,49 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+var engine, world, backgroundImg;
 
-function preload()
-{
-	
+var canvas, angle, tower, ground, cannon;
+var score = 0;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
 }
 
 function setup() {
-	createCanvas(800, 700);
 
-
-	engine = Engine.create();
-	world = engine.world;
-
-	//Crie os Corpos Aqui.
-
-
-	Engine.run(engine);
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
   
-}
+  var options = {
+    isStatic: true
+  }
 
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
+  World.add(world, ground);
+
+  tower = Bodies.rectangle(160, 350, 160, 310, options);
+  World.add(world, tower);
+
+  cannon = new Cannon(180, 110, 130, 100, angle);
+}
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  drawSprites();
- 
+  text (mouseX + "," + mouseY, mouseX, mouseY)
+  image(backgroundImg, 0, 0, width, height);
+
+  Engine.update(engine);
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+  push();
+  imageMode(CENTER);
+  image(towerImage,tower.position.x, tower.position.y, 160, 310);
+  pop();
+
+  cannon.display();
+  textSize(30);
+
+
 }
-
-
-
