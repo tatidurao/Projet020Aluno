6a6f7b473c0171981a3d081685f45f5a876db66d
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2026-02-02 16:01:03 -0300

    Update sketch.js for new canvas and engine configuration
    Refactor sketch.js to initialize a new canvas and engine setup.

--- sketch.js
+++ sketch.js
@@ -1,49 +1,33 @@
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-var engine, world, backgroundImg;
+const Body = Matter.Body;
 
-var canvas, angle, tower, ground, cannon;
-var score = 0;
-
-function preload() {
-  backgroundImg = loadImage("./assets/background.gif");
-  towerImage = loadImage("./assets/tower.png");
+function preload()
+{
+	
 }
 
 function setup() {
+	createCanvas(800, 700);
 
-  canvas = createCanvas(1200, 600);
-  engine = Engine.create();
-  world = engine.world;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Crie os Corpos Aqui.
+
+
+	Engine.run(engine);
   
-  var options = {
-    isStatic: true
-  }
-
-  ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
-  World.add(world, ground);
-
-  tower = Bodies.rectangle(160, 350, 160, 310, options);
-  World.add(world, tower);
-
-  cannon = new Cannon(180, 110, 130, 100, angle);
 }
 
+
 function draw() {
-  text (mouseX + "," + mouseY, mouseX, mouseY)
-  image(backgroundImg, 0, 0, width, height);
-
-  Engine.update(engine);
-  rect(ground.position.x, ground.position.y, width * 2, 1);
-  push();
-  imageMode(CENTER);
-  image(towerImage,tower.position.x, tower.position.y, 160, 310);
-  pop();
-
-  cannon.display();
-  textSize(30);
-
-
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
 }
